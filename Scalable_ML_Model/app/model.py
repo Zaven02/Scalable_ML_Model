5912a9c455b65803e5f22cb32bcbe7bb46f83d13
Zaven <82265339+Zaven02@users.noreply.github.com>
2025-01-29 22:59:05 +0400

    Delete Scalable_ML_Model directory

--- Scalable_ML_Model/app/model.py
+++
@@ -1,9 +0,0 @@
-import joblib
-import numpy as np
-
-def load_model(model_path: str):
-    return joblib.load(model_path)
-
-def make_prediction(model, features: list[float]):
-    features = np.array(features).reshape(1, -1)
-    return model.predict(features)
